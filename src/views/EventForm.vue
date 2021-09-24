8cb6e6cc4d1e1282fd255cf2bfeb260c4d3944dd
munlikaCh <munlika_ch@cmu.ac.th>
2021-09-24 21:51:15 +0700

    8.8+8.9

--- src/views/EventForm.vue
+++ src/views/EventForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Create an event</h1>
-    <form>
+    <form @submit.prevent="saveEvent">
       <label>Category</label>
       <input
         v-model="event.category"
@@ -43,7 +43,7 @@
   </div>
 </template>
 <script>
-
+import EventService from '@/services/EventService.js'
 export default {
   data() {
     return {
@@ -53,6 +53,21 @@ export default {
         description: '',
         location: ''
       }
+    }
+  },
+  methods: {
+    saveEvent() {
+      EventService.saveEvent(this.event)
+        .then((response) => {
+          console.log(response)
+          this.$router.push({
+            name: 'EventLayout',
+            params: { id: response.data.id }
+          })
+        })
+        .catch(() => {
+          this.$router.push('NetworkError')
+        })
     }
   }
 }
